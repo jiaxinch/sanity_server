05978a52551e58e5a2830b3fdbd45c155375d1bb
jiaxin Cheng <jiaxinch@usc.edu>
2017-10-13 22:17:23 -0700

    finish userDAO and changed user class

--- src/User.java
+++ src/User.java
@@ -1,41 +1,13 @@
 
 public class User {
-	private String username;
-	private String email;
-	private String password1;
-	private String password2;
-	private String func;
-	public User(){
-		
-	}
-	public void setFunc(String a){
-		this.func = a;
-	}
-	public void setUsername(String a){
-		this.username = a;
-	}
-	public void setEmail(String a){
-		this.email = a;
-	}
-	public void setPassword1(String a){
-		this.password1 = a;
-	}
-	public void setPassword2(String a){
-		this.password2 = a;
-	}
-	public String getUsername(){
-		return this.username;
-	}
-	public String getEmail(){
-		return this.email;
-	}
-	public String getFunc(){
-		return this.func;
-	}
-	public String getPwd1(){
-		return this.password1;
-	}
-	public String getPwd2(){
-		return this.password2;
+	String username;
+	String email;
+	String password1;
+	String password2;
+	public User(String userName, String email, String psword, String psword2){
+		username=userName;
+		this.email=email;
+		password1=psword;
+		password2=psword2;
 	}
 }
--- src/UserDAO.java
+++ src/UserDAO.java
@@ -1,4 +1,8 @@
 
 public interface UserDAO {
-
+	public void create(String userName, String email, String psword, String psword2);
+	public User getUser(String email);
+	public Boolean logIn(String email, String psword, String psword2);
+	public void updateUserName(String userName, final String email);
+	public void updatePassword(String pswordOld, String psword2Old, String email,String pswordNew, String psword2New);
 }
