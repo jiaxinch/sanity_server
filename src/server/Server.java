85b34df200875e146b9e3b1f1dba4ea22090122f
jiaxin Cheng <jiaxinch@usc.edu>
2017-10-13 23:25:43 -0700

    update manifest and server to run online

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,12 @@
 package server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
 import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketImpl;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import org.json.JSONException;
@@ -53,5 +57,23 @@ public class Server extends WebSocketServer{
 	public void onStart() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public static void main( String[] args ) throws InterruptedException , IOException {
+		WebSocketImpl.DEBUG = true;
+		int port = 9999; // 843 flash policy port
+		Server s = new Server( port );
+		s.start();
+		System.out.println( "ChatServer started on port: " + s.getPort() );
+
+		BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
+		while ( true ) {
+			String in = sysin.readLine();
+			s.broadcast( in );
+			if( in.equals( "exit" ) ) {
+				s.stop();
+				break;
+			}
+		}
 	}
 }
